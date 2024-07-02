32c5de8b3fe683a4650fcd2c46d7ffddeaf726a5
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-07-02 16:46:48 -0700

    chore: use biome formatter indentStyle 'space'

--- ansible/roles/metrics/files/index-generator/src/index.js
+++ ansible/roles/metrics/files/index-generator/src/index.js
@@ -6,49 +6,49 @@ const app = express();
 app.use(json());
 
 async function getFileList() {
-	const fileList = [];
+  const fileList = [];
 
-	const [files] = await storage.bucket("access-logs-summaries-nodejs").getFiles();
-	for (const file of files) {
-		if (!file.name.includes("html")) {
-			fileList.push(file.name);
-		}
-	}
-	return fileList;
+  const [files] = await storage.bucket("access-logs-summaries-nodejs").getFiles();
+  for (const file of files) {
+    if (!file.name.includes("html")) {
+      fileList.push(file.name);
+    }
+  }
+  return fileList;
 }
 
 async function generateIndex() {
-	let fileList = [];
-	fileList = await getFileList();
+  let fileList = [];
+  fileList = await getFileList();
 
-	const baseURL = "https://storage.googleapis.com/access-logs-summaries-nodejs/";
-	let body = "";
+  const baseURL = "https://storage.googleapis.com/access-logs-summaries-nodejs/";
+  let body = "";
 
-	for (file of fileList) {
-		const bodyString = `<p>·<a·href="${baseURL.concat(file)}">·${file}·</a></p>\n`;
-		body += bodyString;
-	}
+  for (file of fileList) {
+    const bodyString = `<p>·<a·href="${baseURL.concat(file)}">·${file}·</a></p>\n`;
+    body += bodyString;
+  }
 
-	const fileContents = `<html>\n<head>\n</head>\n<body>\n${body}</body>\n</html>`;
-	const fileName = "index.html";
-	try {
-		await storage.bucket("access-logs-summaries-nodejs").file(fileName).save(fileContents);
-		console.log(`Upload complete: ${fileName}`);
-	} catch (error) {
-		console.error(`ERROR UPLOADING FILE: ${fileName} - ${error}`);
-	}
+  const fileContents = `<html>\n<head>\n</head>\n<body>\n${body}</body>\n</html>`;
+  const fileName = "index.html";
+  try {
+    await storage.bucket("access-logs-summaries-nodejs").file(fileName).save(fileContents);
+    console.log(`Upload complete: ${fileName}`);
+  } catch (error) {
+    console.error(`ERROR UPLOADING FILE: ${fileName} - ${error}`);
+  }
 }
 
 app.post("/", async (req, res) => {
-	if (req.body.message.attributes.objectId !== "index.html") {
-		await generateIndex();
-	}
-	res.status(200).send();
+  if (req.body.message.attributes.objectId !== "index.html") {
+    await generateIndex();
+  }
+  res.status(200).send();
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-	console.log("Listening on port: ", port);
+  console.log("Listening on port: ", port);
 });
 
 export default app;
